d00125d80b928a868c5e25b6bbdb68ea3147c802
yaboymichaelD <michaeldavidosullivan@gmail.com>
2018-10-24 19:40:42 +0100

    MongoDB added first entry added

---
+++ app_server/models/locations.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose'); 
+
+const registerSchema = new mongoose.Schema({ });
+
+const registerSchema = new mongoose.Schema({
+username: { 
+	type: String, 
+	required: true
+} 
+password: { 
+	type: String,
+	required: true
+
+});
+
+
+
